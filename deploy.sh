db5158cbe09552b14ea1dd3be8da6f0884d29051
CodeFXJ <3010099292@qq.com>
2022-05-02 21:54:13 +0800

    vnotes-src

--- deploy.sh
+++ deploy.sh
@@ -8,19 +8,16 @@ npm run build
 cd public/
 
 # 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# 打包后的文件推送到gitee的gh-pages分支
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
  git push -f git@gitee.com:GiteeFXJ/vnotes.git master:gh-pages
 cd ..
 git add .
 git commit -m 'vnotes-src'
 git push -f origin master
+git push -f git@github.com:CoderFXJ/vnotes.git master
 cd -
